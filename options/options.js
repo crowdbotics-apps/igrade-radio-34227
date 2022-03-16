25dc81f05fb0b0c1ac6be2d1d38c36dc5905f874
Crowdbotics <team@crowdbotics.com>
2022-03-16 00:44:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "igrade_radio_34227",
+  url: "https://igrade_radio_34227.botics.co",
+  api: "https://igrade_radio_34227.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
